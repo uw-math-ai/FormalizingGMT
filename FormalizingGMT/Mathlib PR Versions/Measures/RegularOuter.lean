d408c3ef962b5f316ecf790790f115350d86650f
Ignacio Tejeda <itejeda@uw.edu>
2026-07-09 16:09:58 -0700

    Added todo's for Nathan

--- FormalizingGMT/Mathlib PR Versions/Measures/RegularOuter.lean
+++ FormalizingGMT/Mathlib PR Versions/Measures/RegularOuter.lean
@@ -4,3 +4,9 @@
 **TODO (Theo and Annie)**: Include here the following parts of Project Versions/Measures/Basic, and start a PR to merge them into Mathlib:
 1. definition of regular outer measure
 2. Proposition 1.11.7 from Bogachev's book-/
+
+
+
+/-**TODO (Nathan)**: Lemma: if `μ` is a regular outer measure on a space `X`, then for every `A ⊆ X` there exists a `μ`-measurable set `M` such that `A ⊆ M` and `μ(M \ A) = 0`.-/
+
+/-**TODO (Nathan)**: Lemma: if `μ` is a regular outer measure on a space `X`, then for every `A ⊆ X`, `μ(A)` equals the infimum of `μ(M)` over all `μ`-measurable sets `M` containing `A`.-/
