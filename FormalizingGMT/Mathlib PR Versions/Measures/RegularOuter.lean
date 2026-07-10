c850520ebe7c19360b24469cadc5fc04845694ba
ijtejeda <ijtejedairiondo@gmail.com>
2026-07-10 15:09:52 -0700

    Clarify TODO comments for regular outer measure
    Updated TODO comments to clarify tasks for merging content into Mathlib.

--- FormalizingGMT/Mathlib PR Versions/Measures/RegularOuter.lean
+++ FormalizingGMT/Mathlib PR Versions/Measures/RegularOuter.lean
@@ -3,7 +3,7 @@
 /-
 **TODO (Theo and Annie)**: Include here the following parts of Project Versions/Measures/Basic, and start a PR to merge them into Mathlib:
 1. definition of regular outer measure
-2. Proposition 1.11.7 from Bogachev's book-/
+2. Proposition 1.11.7 from Bogachev's book (if ready; else start a PR with the rest of the content)-/
 
 
 
