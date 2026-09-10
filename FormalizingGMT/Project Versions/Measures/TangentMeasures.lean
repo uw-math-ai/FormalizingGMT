3d283a6a43534006dc1574ae49ff2f7605b9c2f7
tdwag123 <theomeek@uw.edu>
2026-09-10 10:33:31 -0700

    definition that annie and I came up with for tangent measures

--- FormalizingGMT/Project Versions/Measures/TangentMeasures.lean
+++ FormalizingGMT/Project Versions/Measures/TangentMeasures.lean
@@ -1,6 +1,9 @@
-import Mathlib.Analysis.InnerProductSpace.PiL2
+import FormalizingGMT.«Project Versions».Measures.WeakConvergence
 
+open MeasureTheory
 open Topology
+
+variable {n : ℕ}
 
 /--
 The blow-up map `T_{a,r}(x) = (x - a) / r` on Euclidean space `ℝⁿ`.
@@ -11,3 +14,22 @@ def blowUpMap {n : ℕ}
     (x : EuclideanSpace ℝ (Fin n)) :
     EuclideanSpace ℝ (Fin n) :=
   r⁻¹ • (x - a)
+
+/-- `IsTangentMeasure μ ν a` asserts that `ν` is a tangent measure of `μ` at point `a`. -/
+def IsTangentMeasure
+    (μ ν : OuterMeasure (EuclideanSpace ℝ (Fin n)))
+    (hμ : RadonOuterMeasure μ)
+    (a : EuclideanSpace ℝ (Fin n)) : Prop :=
+  RadonOuterMeasure ν ∧ ν ≠ 0 ∧
+  ∃ (r : ℕ → ℝ) (c : ℕ → ℝ),
+    (∀ i, 0 < r i) ∧
+    (∀ i, 0 < c i) ∧
+    Filter.Tendsto r Filter.atTop (𝓝 0) ∧
+    ∃ (h_seq : ∀ i,
+        RadonOuterMeasure
+          (c i • (μ.map (blowUpMap a (r i))))),
+      OuterMeasure.WeaklyConverges
+        (fun i ↦ c i • (μ.map (blowUpMap a (r i))))
+        ν
+        h_seq
+        ‹RadonOuterMeasure ν›
